01dc180064e2b8795ba7a3844febd913c1a94319
paddykka <110348050+paddykka@users.noreply.github.com>
2023-04-07 17:03:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-import cv2
-import cvzone
+import cv2                                            #import opencv module cv2 
+import cvzone                                         #importing cvzone
 from cvzone import FaceDetectionModule as fp
 from cvzone import FaceMeshModule as fm
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)                              #getting the webcam feed by setting 0  for laptop default webcam and by 1 the external webcam
 
 detector = fp.FaceDetector()
-hellow = fm.FaceMeshDetector()
+hellow = fm.FaceMeshDetector()                          #initializing facedetection 
 while True:
     succ , img = cap.read()
     sac = img.copy()
-    img1, bbox1 = detector.findFaces(img)
-    img2 , bbox2 = hellow.findFaceMesh(sac)
+    img1, bbox1 = detector.findFaces(img)               #facedetection
+    img2 , bbox2 = hellow.findFaceMesh(sac)             #facemeshdetection
     cv2.imshow("face", img1)
     cv2.imshow("facemesh", img2)
     cv2.waitKey(1)
